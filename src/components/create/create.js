e4b949326f3562476dd37dff9910114d14c753d3
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2022-01-26 17:28:33 +0700

    [edit]create

--- src/components/create/create.js
+++ src/components/create/create.js
@@ -24,40 +24,42 @@ function Create() {
           {view === 0 && (
             <div>
               <p style={{ marginTop: 20 }}>Select your ticket type</p>
-              <div className="w3-row-padding">
-                <div>
-                  <a
-                    href=""
-                    onClick={(e) => {
-                      setview(1);
-                      e.preventDefault();
-                    }}
-                  >
-                    <div className="w3-half w3-margin-bottom ">
-                      <ul className="w3-ul w3-light-grey w3-center w3-hover-blue-grey">
-                        <li className="w3-padding-16">
-                          <h2>Incident</h2>
-                        </li>
-                      </ul>
-                    </div>
-                  </a>
-                </div>
-                <div>
-                  <a
-                    href=""
-                    onClick={(e) => {
-                      setview(2);
-                      e.preventDefault();
-                    }}
-                  >
-                    <div className="w3-half w3-margin-bottom">
-                      <ul className="w3-ul w3-light-grey w3-center w3-hover-blue-grey">
-                        <li className="w3-padding-16">
-                          <h2>install</h2>
-                        </li>
-                      </ul>
-                    </div>
-                  </a>
+              <div className="w3-container w3-padding-32">
+                <div className="w3-row-padding">
+                  <div>
+                    <a
+                      href=""
+                      onClick={(e) => {
+                        setview(1);
+                        e.preventDefault();
+                      }}
+                    >
+                      <div className="w3-half w3-panel">
+                        <div className="w3-light-grey w3-center w3-hover-blue-grey w3-padding-large">
+                          <div className="w3-padding-64">
+                            <h2 className="w3-padding-64">Incident</h2>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href=""
+                      onClick={(e) => {
+                        setview(2);
+                        e.preventDefault();
+                      }}
+                    >
+                      <div className="w3-half w3-panel">
+                        <div className="w3-light-grey w3-center w3-hover-blue-grey w3-padding-large">
+                          <div className="w3-padding-64">
+                            <h2 className="w3-padding-64">Install</h2>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -106,6 +108,105 @@ function Create() {
               </div>
             </div>
           )}
+          {/* {view === 1 && (
+            <div>
+              <p style={{ marginTop: 20 }}>
+                Fill out the form to check the equipment information!
+              </p>
+              <div class="w3-row-padding w3-light-grey">
+                <form action="/createinformation" target="_blank">
+                  <div className="w3-section text-left">
+                    <label>Name</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Name"
+                      required
+                    />
+                  </div>
+                  <div className="w3-section text-left">
+                    <label>Tel</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Tel"
+                      required
+                    />
+                  </div>
+                  <div className="w3-section text-left">
+                    <label>Email</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Email"
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="w3-button w3-block w3-padding-large w3-margin-bottom w3-blue-grey "
+                  >
+                    Next
+                  </button>
+                </form>
+              </div>
+            </div>
+          )}
+          {view === 2 && (
+            <div>
+              <p style={{ marginTop: 20 }}>
+                Fill out the form to create the new customer information!
+              </p>
+              <div class="w3-row-padding w3-light-grey w3-padding w3-margin">
+                <form action="/createinformation" target="_blank">
+                  <div className="w3-section text-left w3-padding">
+                    <label>Name</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Name"
+                      required
+                    />
+                  </div>
+                  <div className="w3-section text-left w3-padding">
+                    <label>Tel</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Tel"
+                      required
+                    />
+                  </div>
+                  <div className="w3-section text-left w3-padding">
+                    <label>Address</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Address"
+                      required
+                    />
+                  </div>
+                  <div className="w3-section text-left w3-padding">
+                    <label>Email</label>
+                    <input
+                      className="w3-input w3-border"
+                      type="text"
+                      name="Email"
+                      required
+                    />
+                  </div>
+                  <div className="w3-padding">
+                  <button
+                    type="submit"
+                    className="w3-button w3-block w3-padding-large w3-margin-bottom w3-blue-grey "
+                  >
+                    Next
+                  </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          )} */}
         </div>
       </div>
     </div>
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -7,211 +7,153 @@ import Button from "react-bootstrap/Button";
 import TicketModal from "./ticketModal";
 $.DataTable = require("datatables.net");
 
-function Home(){
-
+function Home() {
   useEffect(() => {
     $(document).ready(function () {
       $("#example").DataTable();
     });
   });
 
-    const [modalShow, setModalShow] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
 
-    return (
-      <div>
-        <Sidebar />
-        {/* Top menu on small screens */}
-        <header className="w3-container w3-top w3-hide-large w3-blue-grey w3-xlarge w3-padding">
-          <a
-            href="javascript:void(0)"
-            className="w3-button w3-blue-grey w3-margin-right"
-            onclick="w3_open()"
-          >
-            ☰
-          </a>
-          <span>ENIXER</span>
-        </header>
-        {/* Overlay effect when opening sidebar on small screens */}
+  return (
+    <div>
+      <Sidebar />
+      {/* Top menu on small screens */}
+      <header className="w3-container w3-top w3-hide-large w3-blue-grey w3-xlarge w3-padding">
+        <a
+          href="javascript:void(0)"
+          className="w3-button w3-blue-grey w3-margin-right"
+          onclick="w3_open()"
+        >
+          ☰
+        </a>
+        <span>ENIXER</span>
+      </header>
+      {/* Overlay effect when opening sidebar on small screens */}
+      <div
+        className="w3-overlay w3-hide-large"
+        onclick="w3_close()"
+        style={{ cursor: "pointer" }}
+        title="close side menu"
+        id="myOverlay"
+      />
+      {/* !PAGE CONTENT! */}
+      <div className="w3-main" style={{ marginLeft: 340, marginRight: 40 }}>
+        {/* Header */}
         <div
-          className="w3-overlay w3-hide-large"
-          onclick="w3_close()"
-          style={{ cursor: "pointer" }}
-          title="close side menu"
-          id="myOverlay"
-        />
-        {/* !PAGE CONTENT! */}
-        <div className="w3-main" style={{ marginLeft: 340, marginRight: 40 }}>
-          {/* Header */}
-          <div className="w3-container" style={{ marginTop: 80 }} id="servicemaintain">
-            <h1 className="w3-jumbo w3-text-dark-grey">
-              <b>Services Maintainance</b>
-            </h1>
-          </div>
-          <TicketModal show={modalShow} onHide={() => setModalShow(false)} />
-          {/* Tickets */}
-          <div className="w3-container" id="tickets" style={{ marginTop: 20 }}>
-            <h1 className="w3-xxxlarge w3-text-blue-grey">
-              <b>Tickets</b>
-            </h1>
-            <div class="row">
-              <div class="col-10">
-                <hr
-                  style={{ width: "100%", border: "5px solid grey" }}
-                  className="w3-round"
-                />
-              </div>
-              <div class="col-2">
-                <a href="/create">
-                  <button type="button" class="btn btn-light w3-blue-grey">
-                    Create Ticket
-                  </button>
-                </a> 
-              </div>
+          className="w3-container"
+          style={{ marginTop: 80 }}
+          id="servicemaintain"
+        >
+          <h1 className="w3-jumbo w3-text-dark-grey">
+            <b>Services Maintainance</b>
+          </h1>
+        </div>
+        <TicketModal show={modalShow} onHide={() => setModalShow(false)} />
+        {/* Tickets */}
+        <div className="w3-container" id="tickets" style={{ marginTop: 20 }}>
+          <h1 className="w3-xxxlarge w3-text-blue-grey">
+            <b>Tickets</b>
+          </h1>
+          <div class="row">
+            <div class="col-10">
+              <hr
+                style={{ width: "100%", border: "5px solid grey" }}
+                className="w3-round"
+              />
             </div>
-            <br />
-            <div className="w3-row-padding">
+            <div class="col-2">
+              <a href="/create">
+                <button type="button" class="btn btn-light w3-blue-grey">
+                  Create Ticket
+                </button>
+              </a>
+            </div>
+          </div>
+          <br />
+          <div className="w3-row-padding">
             <div className="w3-margin-bottom">
-            <div className="card-body">
-          <div className="table-responsive">
-            <table id="example" className="display" style={{ width: "100%" }}>
-              <thead>
-                <tr>
-                  <th>TicketId</th>
-                  <th>Subject</th>
-                  <th>Customer</th>
-                  <th>Owner</th>
-                  <th>CreatedBy</th>
-                  <th>CreatedAt</th>
-                  <th>Status</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody className="text-dark">
-                <tr>
-                  <td>1</td>
-                  <td>แอร์ไม่เย็น</td>
-                  <td>บางกอก กอกอก</td>
-                  <td>ช่างเก่ง มากมาย</td>
-                  <td>สมศรี สีทอง</td>
-                  <td>2011/04/25</td>
-                  <td>
-                    <span
-                      className="badge bg-secondary"
-                      style={{ color: "white" }}
-                    >
-                      New
-                    </span>
-                  </td>
-                  <td>
-                    {" "}
-                    <Button
-                      variant="primary"
-                      onClick={() => setModalShow(true)}
-                    >
-                      Details
-                    </Button>
-                  </td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>
-                  {/* <th>TicketId</th>
+              <div className="card-body">
+                <div className="table-responsive">
+                  <table
+                    id="example"
+                    className="display"
+                    style={{ width: "100%" }}
+                  >
+                    <thead>
+                      <tr>
+                        <th>TicketId</th>
+                        <th>Subject</th>
+                        <th>Customer</th>
+                        <th>Owner</th>
+                        <th>CreatedBy</th>
+                        <th>CreatedAt</th>
+                        <th>Status</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-dark">
+                      <tr>
+                        <td>1</td>
+                        <td>แอร์ไม่เย็น</td>
+                        <td>บางกอก กอกอก</td>
+                        <td>ช่างเก่ง มากมาย</td>
+                        <td>สมศรี สีทอง</td>
+                        <td>2011/04/25</td>
+                        <td>
+                          <span
+                            className="badge bg-secondary"
+                            style={{ color: "white" }}
+                          >
+                            New
+                          </span>
+                        </td>
+                        <td>
+                          {" "}
+                          <Button
+                            variant="primary"
+                            onClick={() => setModalShow(true)}
+                          >
+                            Details
+                          </Button>
+                        </td>
+                      </tr>
+                    </tbody>
+                    <tfoot>
+                      <tr>
+                        {/* <th>TicketId</th>
                 <th>Subject</th>
                   <th>Customer</th>
                   <th>Owner</th>
                   <th>CreatedBy</th>
                   <th>CreatedAt</th>
                   <th>Status</th> */}
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
-        </div>
-              {/* <table style={{width:'100%'}}>
-                <thead className="w3-table w3-blue-grey w3-center">
-                    <tr>
-                      <th className="w3-center">
-                        EndUser
-                      </th>
-                      <th className="w3-center">
-                        Ticket No.
-                      </th>
-                      <th className="w3-center">
-                        Serial No.
-                      </th>
-                      <th className="w3-center">
-                        Subject
-                      </th>
-                      <th className="w3-center">
-                        Techinician
-                      </th>
-                      <th className="w3-center">
-                        Status
-                      </th>
-                      <th className="w3-center">
-                        Details
-                      </th>
-                    </tr>
-                </thead>
-                <tbody className="w3-table w3-light-grey w3-center">
-                    <tr>
-                      <td className="w3-center">
-                        ddd
-                      </td>
-                      <td className="w3-center">
-                        ddddd
-                      </td>
-                      <td className="w3-center">
-                        dxdd
-                      </td>
-                      <td className="w3-center">
-                        dxdd
-                      </td>
-                      <td className="w3-center">
-                        dsdd
-                      </td>
-                      <td className="w3-center">
-                        dwdd
-                      </td>
-                      <td className="w3-center">
-                        xs
-                      </td>
-                    </tr>
-                </tbody>
-              </table> */}
-              {/* <ul >
-                <li className="w3-dark-grey w3-xlarge w3-padding-32">Basic</li>
-                <li className="w3-padding-16">Floorplanning</li>
-                <li className="w3-padding-16">10 hours support</li>
-                <li className="w3-padding-16">Photography</li>
-                <li className="w3-padding-16">20% furniture discount</li>
-                <li className="w3-padding-16">Good deals</li>
-                <li className="w3-padding-16">
-                  <h2>$ 199</h2>
-                  <span className="w3-opacity">per room</span>
-                </li>
-              </ul> */}
-            </div>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
-          {/* End page content */}
         </div>
-        {/* W3.CSS Container */}
-        <div
-          className="w3-light-grey w3-container w3-padding-32"
-          style={{ marginTop: 75, paddingRight: 58 }}
-        >
-        </div>
+        {/* End page content */}
       </div>
-    );
+      {/* W3.CSS Container */}
+      <div
+        className="w3-light-grey w3-container w3-padding-32"
+        style={{ marginTop: 75, paddingRight: 58 }}
+      ></div>
+    </div>
+  );
 }
 
 export default Home;
